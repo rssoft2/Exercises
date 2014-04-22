8a7ef08dd356b3b58b9383dee4372589d8ceb351
Frank Rabold <frank.rabold@outlook.com>
2014-04-23 00:31:28 +0200

    Beispiel-Flow mit verschiedenen Providern

---
+++ BindFlowToProvider/BindFlowToProvider.Test/FlowTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BindFlowToProvider.Test
+{
+    [TestClass]
+    public class FlowTest
+    {
+        [TestMethod]
+        public void TestFlowNeedingProviderWithTestMocks()
+        {
+            Flows.FlowNeedingProvider(new MyTestDataProvider(), new MyTestDataSink());
+        }
+    }
+
+    class MyTestDataProvider : ISomeDataProvider
+    {
+        public double GetSomeData()
+        {
+            return 123456;
+        }
+    }
+
+    class MyTestDataSink : ISomeDataSink
+    {
+        public void WriteSomeData(double data)
+        {
+            Assert.AreEqual(61728, data);
+        }
+    }
+}
+
---
+++ BindFlowToProvider/BindFlowToProvider/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace BindFlowToProvider
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Build/Bind
+            ISomeDataProvider someDataProvider = new MyConsoleDataProvider();
+            ISomeDataSink someDataSink = new MyConsoleDataSink();
+            // Run
+            Flows.FlowNeedingProvider(someDataProvider, someDataSink);
+        }
+    }
+
+    public static class Flows
+    {
+        public static void FlowNeedingProvider(ISomeDataProvider someDataProvider, ISomeDataSink someDataSink)
+        {
+            var providedData = someDataProvider.GetSomeData();
+            var calculatedData = Calc(providedData);
+            someDataSink.WriteSomeData(calculatedData);
+        }
+
+        static private double Calc(double data)
+        {
+            return data / 2.0;
+        }
+    }
+
+    public interface ISomeDataProvider
+    {
+        double GetSomeData();
+    }
+
+    public interface ISomeDataSink
+    {
+        void WriteSomeData(double data);
+    }
+
+    class MyConsoleDataProvider : ISomeDataProvider
+    {
+        public double GetSomeData()
+        {
+            double result;
+            return double.TryParse(Console.ReadLine(), out result) ? result : 0;
+        }
+    }
+
+    class MyConsoleDataSink : ISomeDataSink
+    {
+        public void WriteSomeData(double data)
+        {
+            Console.WriteLine("Result: {0}", data);
+        }
+    }
+}
